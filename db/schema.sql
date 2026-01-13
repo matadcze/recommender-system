f0324a602566eaeada6696a5294786544c21e107
Marta Binkowska <109827646+maarbin@users.noreply.github.com>
2026-01-13 12:30:17 +0100

    refactor: user creation and RBAC

--- db/schema.sql
+++ db/schema.sql
@@ -47,6 +47,20 @@ CREATE TABLE IF NOT EXISTS genome_scores (
     PRIMARY KEY (movie_id, tag_id)
 );
 
+-- admin access
 GRANT USAGE ON SCHEMA movielens TO recommender;
-
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA movielens TO recommender;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA movielens TO recommender;
+
+-- user and access for the app
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app_user') THEN
+        CREATE ROLE app_user WITH LOGIN PASSWORD 'appuser';
+    END IF;
+END $$;
+
+GRANT USAGE ON SCHEMA movielens TO app_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA movielens TO app_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA movielens GRANT SELECT ON TABLES TO app_user;
